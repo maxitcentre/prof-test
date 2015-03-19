1134fcefee1a4c8f6106e75c1475151861c88054
maksimtest <mkn_ua@ukr.net>
2015-03-19 16:17:11 +0200

    second commit and push by 19.03.2015

---
+++ MyProject/src/lesson5/Task12.java
@@ -0,0 +1,7 @@
+package lesson5;
+
+public class Task12 {
+	public static void main(String[] args) {
+		System.out.println("OGO GO!");
+	}
+}
---
+++ MyProject/src/lesson6/Task11.java
@@ -0,0 +1,7 @@
+package lesson6;
+
+public class Task11 {
+	public static void main(String[] args) {
+		System.out.println("OGO GO!");
+	}
+}
